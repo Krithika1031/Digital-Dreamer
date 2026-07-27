e74b35fe51a418e879614ca2f559040e38203aa4
Krithika1031 <krithikabhat2006@gmail.com>
2026-07-27 08:30:47 +0530

    Mission 10 final

---
+++ End/script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+    setTimeout(function(){
+
+        document.getElementById("loading").classList.remove("active");
+
+        document.getElementById("success").classList.add("active");
+
+    },3000);
+
+}
